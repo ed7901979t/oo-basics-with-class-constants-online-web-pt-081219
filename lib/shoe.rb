7ef87961f62d59a7729dc93d11190ba6d69a9518
clean-command-1851 <ed7901979t@gmail.com>
2019-08-24 19:22:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,41 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
-  BRAND = []
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-  def brand=(brand)
-    @brand = brand
-    BRAND << brand 
-  end
-
-end
-
-#class Book
- # attr_accessor :author, :page_count  # remove the attr_accessor for genre
-  #attr_reader :title, :genre  # add an attr_reader for #genre
- 
-#  GENRES = []
- 
- # def initialize(title)
-  #  @title = title
-  #end
- 
-  #def turn_page
-   # puts "Flipping the page...wow, you read fast!"
-  #end
- 
-  # create the writer for genre and add the logic for the class constant
-  #def genre=(genre)
-   # @genre = genre
-    #GENRES << genre 
-  #end
-#end
