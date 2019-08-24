e026a7bf55cbf7db56ad6a075f9dec82637a1d4d
clean-command-1851 <ed7901979t@gmail.com>
2019-08-24 18:54:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count,  
-  attr_reader :title, :genre
+  attr_accessor :author, :page_count  # remove the attr_accessor for genre
+  attr_reader :title, :genre  # add an attr_reader for genre
  
   GENRES = []
  
@@ -11,9 +11,10 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-   def genre=(genre)
+ 
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
     @genre = genre
     GENRES << genre 
   end
- 
 end
